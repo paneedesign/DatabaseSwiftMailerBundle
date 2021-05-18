1e556872c736f867ad3fdefa2e1531092b4ff67e
Krzysztof Kowalski <kowalk@users.noreply.github.com>
2021-05-18 15:30:18 +0200

    ManagerRegistry namespace change/update

--- Repository/EmailRepository.php
+++ Repository/EmailRepository.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PaneeDesign\DatabaseSwiftMailerBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Query;
 use PaneeDesign\DatabaseSwiftMailerBundle\Entity\Email;
 
